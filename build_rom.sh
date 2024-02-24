33608fd05d247acec09103f653bd2f10bac97c37
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-02-24 12:06:19 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,14 +4,25 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init
+repo init -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs --depth=1
+git clone https://github.com/EAZYBLACK/judyln_manifest.git --depth=1 -b R .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+cd .repo/local_manifests
+chmod +x setup.sh
+chmod +x rom.sh
+./setup.sh
+./rom.sh
+cd ../..
+git remote add other https://github.com/juleast/android_build_soong.git
+git fetch other
+git cherry-pick 69b1f28
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j$(nproc --all)
+lunch lineage_judyln-eng
+m bacon -j$(nproc --all)
 
 # upload rom
 up(){
@@ -19,4 +30,4 @@ up(){
 	# 14 days, 10 GB limit
 }
 
-up out/target/product/mido/*.zip
+up out/target/product/judyln/*.zip
