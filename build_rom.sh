f6502fe8a24abc1a6387a3a903e7fc4f516a7804
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-02-24 12:32:22 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 # sync rom
+sudo apt install libncurses5 libncursesw5 repo tmux htop git-lfs bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python3 python-is-python3
 repo init
 repo init -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs --depth=1
 git clone https://github.com/EAZYBLACK/judyln_manifest.git --depth=1 -b R .repo/local_manifests
